adee915fad5339f0d1b57d13f0fd2e151243e894
Logan Jones <loganemilyjones@mac.com>
2020-12-15 22:16:18 -0800

    All tests passing

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,7 +1,11 @@
 class Band:
+    
+    instances = []
+
     def __init__(self, name, members=None):
         self.name = name
         self.members = members
+        Band.instances.append(self.name)
 
     def __str__(self):
         return f"The band {self.name}"
@@ -9,11 +13,20 @@ class Band:
     def __repr__(self):
         return f"Band instance. name={self.name}, members={self.members}"
 
+    def play_solos(self):
+        solos = []
+        for member in self.members:
+            solos.append(member.play_solo())
+        return solos
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
 
 class Musician:
     pass
 
-class Guitarist:
+class Guitarist(Musician):
     def __init__(self, name):
         self.name = name
     
@@ -23,8 +36,46 @@ class Guitarist:
     def __repr__(self):
         return f"Guitarist instance. Name = {self.name}"
 
-class Bassist:
-    pass
+    @staticmethod
+    def get_instrument():
+        return "guitar"
 
-class Drummer:
-    pass
+    @staticmethod
+    def play_solo():
+        return "face melting guitar solo"
+
+class Bassist(Musician):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
+    @staticmethod
+    def get_instrument():
+        return "bass"
+
+    @staticmethod
+    def play_solo():
+        return "bom bom buh bom"
+
+class Drummer(Musician):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+
+    @staticmethod
+    def get_instrument():
+        return "drums"
+
+    @staticmethod
+    def play_solo():
+        return "rattle boom crash"
--- tests/test_band.py
+++ tests/test_band.py
@@ -22,12 +22,15 @@ def test_band_repr():
     expected = "Band instance. name=Nirvana, members=[]"
     assert actual == expected
 
+# @pytest.mark.skip("pending")
 def test_guitarist_str():
     joan = Guitarist("Joan Jett")
     actual = str(joan)
     expected = "My name is Joan Jett and I play guitar"
     assert actual == expected
 
+
+# @pytest.mark.skip("pending")
 def test_guitarist_repr():
     joan = Guitarist("Joan Jett")
     actual = repr(joan)
@@ -35,7 +38,7 @@ def test_guitarist_repr():
     assert actual == expected
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_drummer_str():
     sheila = Drummer("Sheila E.")
     actual = str(sheila)
@@ -43,7 +46,7 @@ def test_drummer_str():
     assert actual == expected
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_drummer_repr():
     sheila = Drummer("Sheila E.")
     actual = repr(sheila)
@@ -51,7 +54,7 @@ def test_drummer_repr():
     assert actual == expected
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_bassist_str():
     meshell = Bassist("Meshell Ndegeocello")
     actual = str(meshell)
@@ -59,7 +62,7 @@ def test_bassist_str():
     assert actual == expected
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_bassist_repr():
     meshell = Bassist("Meshell Ndegeocello")
     actual = repr(meshell)
@@ -69,28 +72,28 @@ def test_bassist_repr():
 
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_guitarist():
     jimi = Guitarist("Jimi Hendrix")
     assert jimi.name == "Jimi Hendrix"
     assert jimi.get_instrument() == "guitar"
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_bassist():
     flea = Bassist("Flea")
     assert flea.name == "Flea"
     assert flea.get_instrument() == "bass"
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_drummer():
     ginger = Drummer("Ginger Baker")
     assert ginger.name == "Ginger Baker"
     assert ginger.get_instrument() == "drums"
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_instruments(one_band):
     instruments = ["guitar", "bass", "drums"]
     for i, member in enumerate(one_band.members):
@@ -98,7 +101,7 @@ def test_instruments(one_band):
         assert member.get_instrument() == instruments[i]
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_individual_solos(one_band):
     for member in one_band.members:
         if member.get_instrument() == "guitar":
@@ -109,7 +112,7 @@ def test_individual_solos(one_band):
             assert member.play_solo() == "rattle boom crash"
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_band_members(one_band):
 
     assert len(one_band.members) == 3
@@ -127,7 +130,7 @@ def test_band_members(one_band):
     assert one_band.members[2].name == "Dave Grohl"
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_play_solos_for_whole_band(one_band):
     solos = one_band.play_solos()
     assert len(solos) == 3
@@ -136,7 +139,7 @@ def test_play_solos_for_whole_band(one_band):
     assert solos[2] == "rattle boom crash"
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_to_list():
     assert Band.to_list() == []
     Band("The Nobodies", [])
